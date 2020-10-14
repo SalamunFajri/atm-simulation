84d63f52b2f80acf2ae19a779781db8271a45281
Salamun Fajri <salamun.fajri@mitrais.com>
2020-10-14 21:57:58 +0700

    Add account and bank class

---
+++ src/com/sf/Account.java
@@ -0,0 +1,49 @@
+package com.sf;
+
+public class Account {
+
+    private String name;
+    private String pin;
+    private long balance;
+    private String accountNumber;
+
+    public Account(String name, String pin, long balance, String accountNumber) {
+        this.setName(name);
+        this.setPin(pin);
+        this.setBalance(balance);
+        this.setAccountNumber(accountNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
+
---
+++ src/com/sf/Bank.java
@@ -0,0 +1,25 @@
+package com.sf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+
+    public List<Account> accounts = new ArrayList<>();
+
+    public Bank() {
+        accounts.add(new Account("John Doe","012108", 100, "112233"));
+        accounts.add(new Account("Jane Doe","932012", 30, "112244"));
+    }
+
+    public Account userLogin(String accountNumber, String pin) {
+        for(Account a : this.accounts){
+            if(a.getAccountNumber().compareTo(accountNumber)== 0
+                    && a.getPin().compareTo(pin)== 0){
+                return a;
+            }
+        }
+        return null;
+    }
+
+}
